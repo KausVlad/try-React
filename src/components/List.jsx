67af66e1778a3d181b73a582d0e15e1576549cd6
KausVlad <kausvirtual@gmail.com>
2023-05-03 17:57:44 +0300

    Test

---
+++ src/components/List.jsx
@@ -0,0 +1,28 @@
+/*import { Component } from 'react';
+
+export class List extends Component {
+  //Class component
+  render() {
+    return (
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+        <li>Item 4</li>
+      </ul>
+    );
+  }
+}
+*/
+
+export function List() {
+  // function component
+  return (
+    <ul>
+      <li>Item 1</li>
+      <li>Item 2</li>
+      <li>Item 3</li>
+      <li>Item 4</li>
+    </ul>
+  );
+}
